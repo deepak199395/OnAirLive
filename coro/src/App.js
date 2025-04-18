062490089bd3fdc6a1584610de693292d5e79ba0
Deepak yadav <deepak19939426@gmail.com>
2025-04-18 16:26:13 +0530

    create login

--- coro/src/App.js
+++ coro/src/App.js
@@ -4,12 +4,14 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About';
 import VideoCall from './Pages/VideoCall/VideoCall';
 import Room from './Pages/VideoCall/Room';
+import Login from './Components/Auth/Login';
 function App() {
 
   return (
     <>
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/home" element={<Home/>}></Route>
       <Route path='/about' element={<About/>}></Route>
       <Route path='/video/:id' element={<VideoCall/>}></Route>
       <Route path='/room' element={<Room/>}/>
